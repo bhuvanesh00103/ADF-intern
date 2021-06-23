d122557e688de1a266ea13195bdc9510495cd930
bhuvanesh00103 <bhuvanesh200103@gmail.com>
2021-06-23 10:54:29 +0530

    change

--- read_CSV.py
+++ read_CSV.py
@@ -2,5 +2,5 @@ import pandas as pd
 n = int(input('Enter Number of rows to be selected: '))
 df = pd.read_csv('Sample.csv').iloc[:,:n]
 print(df)
-dict = df.to_dict('dict')
+dict = df.to_dict('list')
 print(dict)
